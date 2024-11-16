a89bce0e2aea19cdacb6ead9a195304e38f7f96e
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-11-16 17:46:30 +0300

    Added middleware and route to index.js, and created BlogCategory and BlogPost models in blog.js

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ const app = express()
 require("dotenv").config()
 const PORT = process.env.PORT || 8000
 /* ----------------------------------- */
-// Middlewares:
-app.use(express.json())
-require('express-async-errors')
+// Middlewares: 
+app.use(express.json()) // ???
+require('express-async-errors') // ??
 /* ----------------------------------- */
 
 // DB connection
 require("./src/configs/dbConnection")
 /* ----------------------------------- */
+
+/* ----------------------------------- */
+
+// Routes
+app.all("/", (req, res)=>{
+    res.send("WELCOME!! :)")
+})
+
 
 
 
--- src/models/blog.js
+++ src/models/blog.js
@@ -1,1 +1,41 @@
 "use strict"
+
+const {Schema, model} = require("mongoose")
+
+const BlogCategorySchema = new Schema({
+    // _id,
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    }
+},{
+    collection: 'BlogCategory',
+    timestamps: true
+})
+
+const BlogPostSchema = new Schema({
+    categoryId: {
+        type: Schema.Types.ObjectId,
+        ref: "BlogCategory",
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true
+    }
+},{
+    collection: 'BlogPost',
+    timestamps: true
+})
+
+module.exports = {
+    BlogCategory: model("BlogCategory", BlogCategorySchema),
+    BlogPost: model("BlogPost", BlogPostSchema),
+}
